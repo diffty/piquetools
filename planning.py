1a3ac64e743974e2bd618b255421dc66a48f12bc
DiFFtY <diffty@gmail.com>
2023-01-31 02:56:34 +0100

    planning module to handle the read and parse the planning gsheet

--- planning.py
+++ planning.py
@@ -1,10 +1,10 @@
-import pickle
 import os
-import sys
 import datetime
 import re
-import json
 import config
+import copy
+
+from typing import List
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -43,31 +43,30 @@ def auth():
     
     return creds
 
-# http://localhost:60520/?state=C1AIYAfYzCzIuwS57yNY81EvDPrMwA&code=4/0AWtgzh4vJtOa29ZYcHZYQyOGRdFqby0RrfB7Fg63LgKstv1le6cRjBqSc3ow93d-To285Q&scope=https://www.googleapis.com/auth/spreadsheets.readonly
 
 import locale
 locale.setlocale(locale.LC_TIME,'')
 
 import datetime
 
-def main():
+
+def get_timetable_from_sheet(sheet_id, sheet_name):
     creds = auth()
 
     curr_parse_day = None
     curr_parse_month = None
-    curr_parse_time = None
+
+    interval_list = []
+    curr_interval = None
 
     try:
         service = build('sheets', 'v4', credentials=creds)
 
-        # The ID and range of a sample spreadsheet.
-        SAMPLE_SPREADSHEET_ID = config.SHEET_ID
-        SAMPLE_RANGE_NAME = "'Semaine 23/01-29/01'!A:C"
-
         # Call the Sheets API
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
+        result = sheet.values().get(spreadsheetId=sheet_id,
+                                    range=f"'{sheet_name}'!A:C").execute()
+
         values = result.get('values', [])
 
         for row in values:
@@ -95,11 +94,65 @@ def main():
                 if len(row) > 1:
                     title = row[1]
                     streamer = row[2]
-                    print(f"{curr_parse_day}/{curr_parse_month}: {curr_parse_interval[0]}:{curr_parse_interval[1]} -> {curr_parse_interval[2]}:{curr_parse_interval[3]}: {title} ({streamer})")
+
+                    start_date = datetime.datetime(year=datetime.datetime.now().year, month=int(curr_parse_month), day=int(curr_parse_day), hour=int(curr_parse_interval[0]), minute=int(curr_parse_interval[1]))
+                    delta_date = datetime.timedelta(hours=int(curr_parse_interval[2])-int(curr_parse_interval[0]), minutes=int(curr_parse_interval[3])-int(curr_parse_interval[1]))
+
+                    if delta_date.total_seconds() < 0:
+                        delta_date += datetime.timedelta(days=1)
+
+                    if curr_interval is not None and title != curr_interval["title"] and streamer != curr_interval["streamer"]:
+                        interval_list.append(curr_interval)
+                        curr_interval = None
+
+                    if curr_interval is None:
+                        curr_interval = {
+                            "start": None,
+                            "end": None,
+                            "title": None,
+                            "streamer": None,
+                        }
+
+                    if curr_interval["start"] is None:
+                        curr_interval["start"] = start_date
+                        curr_interval["end"] = copy.deepcopy(start_date)
+                        curr_interval["title"] = title
+                        curr_interval["streamer"] = streamer
+
+                    curr_interval["end"] += delta_date
+                else:
+                    if curr_interval is not None:
+                        interval_list.append(curr_interval)
+                        curr_interval = None
 
     except HttpError as err:
         print(err)
 
+    if curr_interval is not None:
+        interval_list.append(curr_interval)
+
+    return interval_list
+
+
+def find_current_show(t: datetime.datetime) -> dict:
+    timetable = get_timetable_from_sheet(config.SHEET_ID, 'Semaine 30/01-05/02')
+    for show in timetable:
+        if show["start"] < t and show["end"] > t:
+            return show
+
+
+def find_next_show(t: datetime.datetime) -> dict:
+    timetable = get_timetable_from_sheet(config.SHEET_ID, 'Semaine 30/01-05/02')
+    for i, show in enumerate(timetable):
+        if show["start"] > t:
+            return timetable[i]
+
 
 if __name__ == '__main__':
-    main()
+    timetable = get_timetable_from_sheet(config.SHEET_ID, 'Semaine 30/01-05/02')
+    t = datetime.datetime(year=2023, month=1, day=30, hour=19, minute=14)
+
+    for interval in timetable:
+        if interval["start"] < t and interval["end"] > t:
+            print(interval)
+
